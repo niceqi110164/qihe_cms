42420215e551fd6d3d235760a2957831a095cde1
xiaoniubei <15210690703@163.com>
2020-06-15 15:33:26 +0800

    managers=> 修改doAdd操作

--- routes/admin/manager.js
+++ routes/admin/manager.js
@@ -28,40 +28,34 @@ router.post('/doAdd', async (ctx)=>{
         status:"1",
         last_time:new Date()
     };
+
     json.username = ctx.request.body.username;
-    json.password = tools.getMd5(ctx.request.body.password);
+   // json.password = tools.getMd5(ctx.request.body.password);
+    json.password = ctx.request.body.password;
     json.rpassword = ctx.request.body.rpassword;
     /**2.验证表单数据是否合法*/
-    if(!/[a-zA-Z\u4e00-\u9fa5]{4,20}/.test(json.username)){
-        await ctx.render("admin/error/error",{
-            message: '用户名不合法',
-            redirect: ctx.state.__HOST__ + '/admin/manager/add'
-        })
+    if(!/^([a-zA-Z\u4E00-\u9FA5]*?)$/.test(json.username)){
+        /**返回失败数据*/
+        ctx.body = {'message':'用户名不合法',success:false};
     }else if(ctx.request.body.password.length<6 ||ctx.request.body.password.length>20){
-        await ctx.render('admin/error', {
-            message: '密码长度为6~20为之间',
-            redirect: ctx.state.__HOST__ + '/admin/manager/add'
-        });
+        /**返回失败数据*/
+        ctx.body = {'message':'密码长度为6~20',success:false};
     }else if(ctx.request.body.password !== ctx.request.body.rpassword){
-        await ctx.render('admin/error',{
-            message:'两次输入的密码不一致',
-            redirect:ctx.state.__HOST__+'/admin/manager/add'
-        });
+        /**返回失败数据*/
+        ctx.body = {'message':'两次密码不一致',success:false};
     }else{
         /**3.在数据库查询当前要增加的管理员是否存在*/
         let managerResult = await DB.find("managers",{"username":json.username});
         if(managerResult.length>0){
-            await ctx.render('admin/error',{
-                message:'用户名已存在',
-                redirect:ctx.state.__HOST__+'/admin/manager/add'
-            })
+            /**返回失败数据*/
+            ctx.body = {'message':'用户已存在',success:false};
         }else{
             /**4.增加管理员*/
             let insertResult = await DB.insert('managers',json);
             //console.log(insertResult);
             if(insertResult.result.ok === 1){
-                //await ctx.redirect('/admin/manager/index')
-                await ctx.redirect(ctx.state.__HOST__+'/admin/manager')
+                /**返回成功数据*/
+                ctx.body = {'message':'添加成功',success:true};
             }
         }
     }
